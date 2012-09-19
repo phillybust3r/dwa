cf7ef9797154c367af900fc978833395a36c9115
Philipp Bantillo <phil_bantillo@hotmail.com>
2012-09-18 21:59:18 -0400

    updated index.php
    added more info

--- p1/index.php
+++ p1/index.php
@@ -1,4 +1,37 @@
-Philipp Bantillo
-Small Orange
+<!DOCTYPE html>
+<html>
+<head>
+<title>CSCI E-75 P1</title>
+</head>
 
-is this my first assignment
+<body>
+
+	<h1>Philipp Cortez Bantillo</h1>
+	<div>
+		<h2>Bio:</h2>
+		<p>I am a C++ software engineer for AER - a weather company out in Lexington, MA. I am currently working on updating the software for the GOES-R weather satellite.</p>
+	</div>
+
+	<div>
+		<h2>Web host:</h2>
+		<p>
+			<a href="http://asmallorange.com">A Small Orange</a>
+		</p>
+		<p>Tiny plan.</p>
+	</div>
+
+	<div>
+		<h2>Code editor:</h2>
+		<p>CODA</p>
+	</div>
+
+	<div>
+		<h2>Git repository:</h2>
+		<p>
+			<a href="https://github.com/phillybust3r/dwa">https://github.com/phillybust3r/dwa</a>
+		</p>
+	</div>
+</body>
+</html>
+
+
