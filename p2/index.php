8ccaa90a536b5d7c88d07c8aeb5270504c0a371b
Philipp Bantillo <phil_bantillo@hotmail.com>
2012-09-26 18:01:55 -0400

    first PHP

---
+++ p2/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+
+	<head>
+		<title></title>
+		<?php
+			$square = 4 * 4;
+		?>
+		
+	</head>
+	
+	<body>
+		The square of 4 is <?=$square?>
+
+
+	</body>
+</html>
